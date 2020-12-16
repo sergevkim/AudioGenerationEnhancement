12e0d33458f7c3dae3857ea2d9c1adb3da8873fa
neTpyLUKa <kirillking123@yandex.ru>
2020-12-17 02:25:18 +0300

    msg

--- agenh/trainer/trainer.py
+++ agenh/trainer/trainer.py
@@ -68,22 +68,16 @@ class Trainer:
         losses = list()
 
         for batch_idx, batch in enumerate(tqdm.tqdm(train_dataloader)):
-            loss = model.training_step(
-                batch=batch,
-                batch_idx=batch_idx,
-            )
+            loss = model.training_step(batch, batch_idx)
             losses.append(loss.item())
             loss.backward()
-            utils.clip_grad_norm_(
-                parameters=model.parameters(),
-                max_norm=10,
-            )
+            utils.clip_grad_norm_(parameters=model.parameters(), max_norm=10)
 
             for optimizer in optimizers:
                 optimizer.step()
                 optimizer.zero_grad()
 
-            model.training_step_end(batch_idx=batch_idx)
+            model.training_step_end()
 
         average_loss = sum(losses) / len(losses)
 
@@ -104,12 +98,9 @@ class Trainer:
         losses = list()
 
         for batch_idx, batch in enumerate(tqdm.tqdm(val_dataloader)):
-            loss = model.validation_step(
-                batch=batch,
-                batch_idx=batch_idx,
-            )
+            loss = model.validation_step(batch, batch_idx)
             losses.append(loss.item())
-            model.validation_step_end(batch_idx=batch_idx)
+            model.validation_step_end()
 
         average_loss = sum(losses) / len(losses)
 
@@ -168,14 +159,11 @@ class Trainer:
         predicts = list()
 
         for batch_idx, batch in enumerate(test_dataloader):
-            predict = model.test_step(
-                batch=batch,
-                batch_idx=batch_idx,
-            )
+            predict = model.test_step(batch, batch_idx)
             predicts.append(predict)
-            model.test_step_end(batch_idx=batch_idx)
+            model.test_step_end()
 
-        model.test_epoch_end(epoch_idx=epoch_idx)
+        model.test_epoch_end()
 
         return predicts
 
