f03854d8b79ef2c0bb8ad1c94d5dbe1355348a9e
yakovdom <yack_domnicky@mail.ru>
2020-12-17 06:24:56 +0000

    Autoencoder draft: WIP

--- agenh/models/lstm.py
+++ agenh/models/lstm.py
@@ -10,10 +10,11 @@ class RNNGenerator(nn.Module):
     def __init__(self, config):
         super(RNNGenerator, self).__init__()
         self.learning_rate = 3e-4
+        self.encoder = nn.Embedding(256, 32)
         self.mu_law_encoder = torchaudio.transforms.MuLawEncoding(256)
         self.mu_law_decoder = torchaudio.transforms.MuLawDecoding(256)
         self.lstm = nn.LSTM(
-            input_size=1,
+            input_size=32,
             hidden_size=256,
             num_layers=2
         )
@@ -21,6 +22,10 @@ class RNNGenerator(nn.Module):
         self.train_loss = nn.CrossEntropyLoss()
 
     def forward(self, x, hidden):
+        x = self.encoder(x)
+
+        #print('x SHAPE', x.shape)
+        #print('hidden SHAPE', hidden[0].shape)
         out, hidden = self.lstm(x, hidden)
         # out = self.dropout(out)
         # x = self.mu_law_decoder(out)
@@ -28,14 +33,14 @@ class RNNGenerator(nn.Module):
         return out, hidden
 
     def init_hidden(self, batch_size=1):
-        return (torch.zeros(2, batch_size, 256, requires_grad=True).to(self.device),
-               torch.zeros(2, batch_size, 256, requires_grad=True).to(self.device))
+        return [torch.zeros(2, batch_size, 256, requires_grad=True).to(self.device),
+               torch.zeros(2, batch_size, 256, requires_grad=True).to(self.device)]
 
     def training_step(self, batch, bacth_idx):
         hidden = self.init_hidden(batch.shape[0])
         batch = batch.to(self.device)
-        batch = torch.transpose(self.mu_law_encoder(batch), 0, 1).unsqueeze(2).type(torch.float)
-        gt = nn.functional.pad(batch, (0, 0, 0, 0, 1, 0))[:-1]
+        batch = torch.transpose(self.mu_law_encoder(batch), 0, 1)#.unsqueeze(2).type(torch.float)
+        gt = nn.functional.pad(batch, (0, 0, 1, 0))[:-1]
         print('x SHAPE', batch.shape)
         print('hidden SHAPE', hidden[0].shape)
         res, _ = self.forward(batch, hidden)
@@ -44,21 +49,18 @@ class RNNGenerator(nn.Module):
 
         # res = res.transpose(1, 2).contiguous().view((-1, 256))
         res = res.contiguous().view((-1, 256))
-        gt = gt.squeeze(2).view((-1)).type(torch.long)
+        #gt = gt.squeeze(2).view((-1)).type(torch.long)
+        gt = gt.view((-1)).type(torch.long)
         print('res SHAPE', res.shape)
         print('gt SHAPE', gt.shape)
-
-        lll = nn.L1Loss()
-        l1 = lll(torch.argmax(res, dim=1, keepdim=True).type(torch.float), gt.type(torch.float))
-        print('\n\nL1: ', l1.item())
         return self.train_loss(res, gt)
 
     def stupid_inference(self, wav):
         batch = wav.unsqueeze(0)
         hidden = self.init_hidden(batch.shape[0])
         batch = batch.to(self.device)
-        batch = torch.transpose(self.mu_law_encoder(batch), 0, 1).unsqueeze(2).type(torch.float)
-        gt = nn.functional.pad(batch, (0, 0, 0, 0, 1, 0))[:-1]
+        batch = torch.transpose(self.mu_law_encoder(batch), 0, 1)#.unsqueeze(2).type(torch.float)
+        gt = nn.functional.pad(batch, (0, 0, 1, 0))[:-1]
         print('x SHAPE', batch.shape)
         print('hidden SHAPE', hidden[0].shape)
         res, _ = self.forward(batch, hidden)
@@ -71,21 +73,26 @@ class RNNGenerator(nn.Module):
         return res
 
     def inference(self, wav):
-        batch = wav[:1].unsqueeze(0)
+        batch = wav[:1]
         hidden = self.init_hidden(batch.shape[0])
-        batch = batch.to(self.device)
-        batch = torch.transpose(self.mu_law_encoder(batch), 0, 1).unsqueeze(2).type(torch.float)
+        hidden[0] += torch.zeros_like(hidden[0]).normal_(mean=0, std=0.02).to(self.device)
+        hidden[1] += torch.zeros_like(hidden[1]).normal_(mean=0, std=0.02).to(self.device)
+        
+        batch = batch.to(self.device) #.type(torch.long)
+        batch = self.mu_law_encoder(batch).unsqueeze(0)
         answer = wav[:1].to(self.device)
+        print('RES', batch, self.mu_law_encoder(wav[0]))
         for i in range(wav.shape[0]):
-            #print('batch.shape', batch.shape)
-            #print('hidden.shape', hidden[0].shape)
-            batch = batch.type(torch.float)
             batch, hidden = self.forward(batch, hidden)
-            #if i == 1:
+            # batch, hidden = self.forward(self.mu_law_encoder(wav[i:i + 1]).to(self.device).unsqueeze(0), hidden)
             #    print('RESS', batch)
-            batch = torch.argmax(batch, dim=2, keepdim=True)
-            #print('TMP.shape', batch.shape)
-            answer = torch.cat((answer, batch[:, :, -1].squeeze(0)), dim=-1)
+            batch = torch.argmax(batch, dim=2)
+            if i < 100:
+                print('RES', batch, self.mu_law_encoder(wav[i + 1]))
+            tmp = batch[:, -1]
+            #print('answer', answer.shape)
+            #print('tmp', tmp.shape)
+            answer = torch.cat((answer, tmp), dim=-1)
 
         res = self.mu_law_decoder(answer)
         return res
