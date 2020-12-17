d228d1816f147812666038f1ffc1760000684570
neTpyLUKa <kirillking123@yandex.ru>
2020-12-17 11:03:24 +0300

    add kernel

--- restart.sh
+++ restart.sh
@@ -8,4 +8,6 @@ docker build -t agenh .
 
 docker run -p 8000:8000 --name agenh -it --entrypoint bash -d agenh  
 
+docker exec -it agenh python -m ipykernel install --user --name=myvirtualenv
+
 docker exec -it agenh python manage.py runserver 0.0.0.0:8000
